38a978ceddffc263fb8b0adf8d0f762eeb8c9d92
Isak <isaktlarsson99@gmail.com>
2024-11-02 18:10:03 -0500

    Non-functional, no errors

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -4,16 +4,20 @@ const router = Router();
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
+/*
+    ALL method parameters have been marked with an underscore.
+*/
+
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', (_req, _res) => {
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req, _res) => {});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (_req, _res) => {});
 
 export default router;
--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,10 +1,15 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
+
+// Tried this, nearly identical to mod 9 activity 27; doesn't work.
+// router.get('/', (_req: Request, res: Response) => {
+//     res.sendFile(path.join(__dirname, "../../../client/index.html"))
+// })
 
 export default router;
