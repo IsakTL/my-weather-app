c67215098399f427eeab81a809947cd1e153f8fc
Isak <isaktlarsson99@gmail.com>
2024-12-23 10:16:10 -0600

    Update broken method in HistoryService

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -3,10 +3,11 @@
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  private async read() {
-    const h = localStorage.getItem('searchHistory.json')!;
-    return JSON.parse(h);
-  }
+  /** Below method does not work, due to no searchHistory.json file being created and the method itself not being called. **/
+  // private async read() {
+  //   const h = localStorage.getItem('searchHistory.json')!;
+  //   return JSON.parse(h);
+  // }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
